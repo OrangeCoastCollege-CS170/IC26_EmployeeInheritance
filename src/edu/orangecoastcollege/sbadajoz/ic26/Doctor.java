a8cd9529c2bd55027224d6e9ad61e4f760cdb10b
Badajoz <sbadajoz@student.cccd.edu>
2016-12-07 12:35:26 -0800

    Format code

--- src/edu/orangecoastcollege/sbadajoz/ic26/Doctor.java
+++ src/edu/orangecoastcollege/sbadajoz/ic26/Doctor.java
@@ -6,16 +6,17 @@ December 7, 2016
 
 IC26
 */
-public class Doctor extends Employee{
-	public Doctor(String honorific, String name, double annualSalary, int yearHired, String id) {
+public class Doctor extends Employee {
+	public Doctor(String honorific, String name, double annualSalary,
+			int yearHired, String id) {
 		super(honorific, name, yearHired, id);
 		super.setAnnualSalary(annualSalary);
 	}
-	
+
 	public Doctor(Doctor other) {
-		super(other.getHonorific(), other.getName(), other.getYearHired(), other.getIDNumber());
+		super(other.getHonorific(), other.getName(), other.getYearHired(),
+				other.getIDNumber());
 		super.setAnnualSalary(other.getAnnualSalary());
 	}
-	
-	
+
 }
