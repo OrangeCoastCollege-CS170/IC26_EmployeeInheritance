07946efa12bf9ec64971769464cfb3cc63bb38f4
Badajoz <sbadajoz@student.cccd.edu>
2016-12-07 12:44:19 -0800

    Fix functionality to satisfy requirements

--- src/edu/orangecoastcollege/sbadajoz/ic26/Doctor.java
+++ src/edu/orangecoastcollege/sbadajoz/ic26/Doctor.java
@@ -7,16 +7,77 @@ December 7, 2016
 IC26
 */
 public class Doctor extends Employee {
-	public Doctor(String honorific, String name, double annualSalary,
-			int yearHired, String id) {
-		super(honorific, name, yearHired, id);
-		super.setAnnualSalary(annualSalary);
+	String mSpecialty;
+	double mOfficeFee;
+
+	public Doctor(String name, double annualSalary, int yearHired, String id,
+			String specialty, double officeFee) {
+		super("Dr.", name, yearHired, id);
+		setAnnualSalary(annualSalary);
+		mSpecialty = specialty;
+		mOfficeFee = officeFee;
 	}
 
 	public Doctor(Doctor other) {
 		super(other.getHonorific(), other.getName(), other.getYearHired(),
 				other.getIDNumber());
-		super.setAnnualSalary(other.getAnnualSalary());
+		setAnnualSalary(other.getAnnualSalary());
+		mSpecialty = other.mSpecialty;
+		mOfficeFee = other.mOfficeFee;
+	}
+
+	public String getSpecialty() {
+		return mSpecialty;
+	}
+
+	public void setSpecialty(String specialty) {
+		this.mSpecialty = specialty;
+	}
+
+	public double getOfficeFee() {
+		return mOfficeFee;
+	}
+
+	public void setOfficeFee(double officeFee) {
+		this.mOfficeFee = officeFee;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		long temp;
+		temp = Double.doubleToLongBits(mOfficeFee);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result
+				+ ((mSpecialty == null) ? 0 : mSpecialty.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Doctor other = (Doctor) obj;
+		if (Double.doubleToLongBits(mOfficeFee) != Double
+				.doubleToLongBits(other.mOfficeFee))
+			return false;
+		if (mSpecialty == null) {
+			if (other.mSpecialty != null)
+				return false;
+		} else if (!mSpecialty.equals(other.mSpecialty))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Doctor [" + super.toString() + ", specialty=" + mSpecialty
+				+ ", officeFee=" + mOfficeFee + "]";
 	}
 
 }
